8d1557a64ddd4b61843790ce6b0c6aaff44db09a
ryaman2005 <girrajbohra50@gmail.com>
2026-05-02 12:58:23 +0530

    revert(domain): roll back custom domain integration and stick to basic Brevo setup

--- server/services/mailService.js
+++ server/services/mailService.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 
 // Brevo requires sending from the verified email
-const SENDER_EMAIL = process.env.SENDER_EMAIL || "support@rentkaro.shop";
+const SENDER_EMAIL = "girrajbohra50@gmail.com";
 
 async function sendEmail({ toEmail, subject, html }) {
   if (!BREVO_API_KEY) {
